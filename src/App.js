1581e03790ea0a4d7c2a4e779c3dea12eb6f9338
abdulshakur <abdulshakur486@gmail.com>
2021-12-25 18:20:27 +0300

    Changed the slash sign to asterik sign on route path

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { Routes, Route, useNavigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Home from "./container/Home";
 
 function App() {
-  const navigate = useNavigate();
   return (
     <Routes>
       <Route index path="/login" element={<Login />} />
-      <Route path="/*" element={<Home />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }

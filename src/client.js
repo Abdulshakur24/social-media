b64b690d05888d34d406ea4e2b0fadbd0b29caff
abdulshakur <abdulshakur486@gmail.com>
2021-12-25 18:21:22 +0300

    Added sanitiy configurations

--- src/client.js
+++ src/client.js
@@ -1,14 +1,23 @@
 import sanityClient from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
-export const client = sanityClient({
+export const writeClient = sanityClient({
   projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
   dataset: "production",
-  apiVersion: "2021-11-16",
-  useCdn: true,
   token: process.env.REACT_APP_SANITY_TOKEN,
+  useCdn: false,
+  apiVersion: "2021-08-31",
+  ignoreBrowserTokenWarning: true,
 });
 
-const builder = imageUrlBuilder(client);
+export const readClient = sanityClient({
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: "production",
+  useCdn: true,
+  apiVersion: "2021-08-31",
+  ignoreBrowserTokenWarning: true,
+});
+
+const builder = imageUrlBuilder(writeClient);
 
 export const urlFor = (source) => builder.image(source);

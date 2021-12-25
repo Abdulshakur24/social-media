56efdda3fc58115454c6af9655920711d01fbdbc
abdulshakur <abdulshakur486@gmail.com>
2021-12-25 18:23:47 +0300

    Social Media project challenge completed!

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      animation: { "spin-slow": "spin 3s linear infinite" },
+    },
   },
   plugins: [],
 };
